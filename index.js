c5345a5e87606d89f0c4f19055396e69d8afaf3f
EnriqueGS88 <enrique.gzs@gmail.com>
2022-11-08 23:51:45 +0100

    save scraped data into csv

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const puppeteer = require('puppeteer');
 const { convertArrayToCSV } = require('convert-array-to-csv');
 const converter = require('convert-array-to-csv');
 const fs = require('fs-extra');
-const listOfDates = require('./listOfDates');
-const timeoutsValues = require('./timeoutsValues');
+// const listOfDates = require('./listOfDates');
+// const timeoutsValues = require('./timeoutsValues');
 require('dotenv').config();
 const USERNAME = process.env.ETHERSCAN_APIKEY;
 
@@ -14,7 +14,7 @@ console.log(USERNAME)
 debugger
 // URLs to scrape
 // const cryptofeesURL = `https://cryptofees.info/history/2021-11-02`;
-const cryptofeesURL = `https://cryptofees.info/history/`;
+const crunchbase = `https://www.crunchbase.com/organization/rapidapi`;
 
 // const header = ['date', 'protocol', 'daily_fees_usd'];
 
@@ -44,42 +44,44 @@ async function getPrices(){
         }
     })
 
+    await page.goto(crunchbase);
+
+
     let historyPrices = [];
 
     // Loop to get the protocol name and fees from 2 selectors
     // Fist loop to go through all the dates
-    for (let i = 0; i<listOfDates.length-1; i++) {
+    // for (let i = 0; i<listOfDates.length-1; i++) {
 
-        await page.goto(cryptofeesURL.concat(listOfDates[i]));
-        await page.waitForTimeout(timeoutsValues[i]);
+    //     await page.waitForTimeout(timeoutsValues[i]);
 
-        // scrape the top 10 protocols in each date
-        let topList = 10;
+    //     // scrape the top 10 protocols in each date
+    //     let topList = 10;
 
-        // This loop to go through the top protocols
-        // i = picks the date from listOfDates
-        for ( let k = 2; k < topList+2 ; k++ ) {
+    //     // This loop to go through the top protocols
+    //     // i = picks the date from listOfDates
+    //     for ( let k = 2; k < topList+2 ; k++ ) {
             
-            const nameSelector = `#__next > div > main > div.jsx-2013905549.list > a:nth-child(${k}) > div.jsx-166918656.name > div`;
-            const feesSelector = `#__next > div > main > div.jsx-2013905549.list > a:nth-child(${k}) > div:nth-child(2)`;
+    //         const nameSelector = `#__next > div > main > div.jsx-2013905549.list > a:nth-child(${k}) > div.jsx-166918656.name > div`;
+    //         const feesSelector = `#__next > div > main > div.jsx-2013905549.list > a:nth-child(${k}) > div:nth-child(2)`;
 
-            let date = listOfDates[i];
-            let protocol = await page.$eval(nameSelector, element => element.innerText);
-            let formatProtocol = protocol.replaceAll(' ', '_').replaceAll('.', '_');
-            let fees = await page.$eval(feesSelector, element => element.innerText);
-            let formatted_fees = fees.replaceAll(',', '').replace('$', '');
-            let fees_usd = formatted_fees.slice(0, (formatted_fees.length-3));
+    //         let date = listOfDates[i];
+    //         let protocol = await page.$eval(nameSelector, element => element.innerText);
+    //         let formatProtocol = protocol.replaceAll(' ', '_').replaceAll('.', '_');
+    //         let fees = await page.$eval(feesSelector, element => element.innerText);
+    //         let formatted_fees = fees.replaceAll(',', '').replace('$', '');
+    //         let fees_usd = formatted_fees.slice(0, (formatted_fees.length-3));
 
-            // Use the following snippet instead to prepare a CSV output
-            let subArray = [];
-            subArray.push(date);
-            subArray.push(formatProtocol);
-            subArray.push(fees_usd);            
-            historyPrices.push(subArray);
+    //         // Use the following snippet instead to prepare a CSV output
+    //         let subArray = [];
+    //         subArray.push(date);
+    //         subArray.push(formatProtocol);
+    //         subArray.push(fees_usd);            
+    //         historyPrices.push(subArray);
             
-        }    
-        // console.log(historyPrices);
-    }
+    //     }    
+    //     // console.log(historyPrices);
+    // }
 
     // Build string output
     const outputScrape = "module.exports = " + JSON.stringify( historyPrices ) + " ";
